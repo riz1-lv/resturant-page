0445260e98f7e192ab3c37126ad57333663e53e2
riz1-lv <riz.shkh1@gmail.com>
2020-12-11 18:30:43 -0500

    made navebar transparent when at top of page

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import Navbar from 'react-bootstrap/Navbar';
-
+import './NavigationBar.css';
 
 
 const NavigationBar = () => {
+  
+  const [scrollNav,setScrollNav] = useState(false);
+
+  const changeNav = () =>{
+    if(window.scrollY >= 80)
+      setScrollNav(true);
+    else{
+      setScrollNav(false);
+    }  
+  }
+  
+  useEffect(()=>{
+    window.addEventListener('scroll', changeNav)
+  },[scrollNav]);
+
   return (
     <div>
-      
-      <Navbar fixed="top" variant="dark" bg="dark" expand="lg">
-      <Navbar.Brand >React-Bootstrap</Navbar.Brand>
+      <Navbar className={scrollNav?'navbar active':'navbar'} fixed="top" variant="dark" expand="lg">
+        <Navbar.Brand className="navLogo" href='/' >React-Bootstrap</Navbar.Brand>
       </Navbar>
-      
     </div>
     
   )
