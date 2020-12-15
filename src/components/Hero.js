511165fb1c2e09e167e34110a2366d15bb685412
riz1-lv <riz.shkh1@gmail.com>
2020-12-15 17:41:11 -0500

    added styling and fixes to hero card

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,12 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div>
       <div id="heroContainer">
+        <div id='heroBg'>
+          <video id="heroVid" src="/videos/video.mp4" autoPlay loop muted />
+        </div>
+        <h1 id="title">EXPERIANCE GORMET COOKING</h1>
       </div>
-    </div>
   )
 }
 
