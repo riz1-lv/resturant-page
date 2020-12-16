f7ed597d6239cfa168cbdec05b180947c5a50922
riz1-lv <riz.shkh1@gmail.com>
2020-12-16 17:20:41 -0500

    added resturant location info, fixed video responsiveness

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,16 @@ const Hero = () => {
         <div id='heroBg'>
           <video id="heroVid" src="/videos/video.mp4" autoPlay loop muted />
         </div>
-        <h1 id="title">EXPERIANCE GORMET COOKING</h1>
+        <h1 id="title">EXPERIENCE GORMET CUISINE</h1>
+        <div id="mainText">
+        <p id="contactInfo">
+          100 10th Street
+          <hr id="line"/>
+          San Francisco CA 12345
+          <hr id="line"/>
+          (555) 555-5555
+        </p>
+        </div>
       </div>
   )
 }
